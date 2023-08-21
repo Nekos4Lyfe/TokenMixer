fb22bfbcd8075d14b5d13975cea7e989eb5f5331
Nekos4Lyfe <130230016+Nekos4Lyfe@users.noreply.github.com>
2023-08-21 14:13:54 +0200

    ComfyUI compatibility framework

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 ####
-from .lib.toolbox.comfy_compatibility import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from lib.install.dependencies import install_tokenmixer_dependencies
+install_tokenmixer_dependencies()
+
+from .comfyUI.compatibility import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
